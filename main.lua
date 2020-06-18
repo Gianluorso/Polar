92daa896218a32ac46f86b9856534ee4336706f5
Gianlu <g1an@ymail.com>
2020-06-18 19:07:48 +0200

    main.lua commentato

--- main.lua
+++ main.lua
@@ -1,19 +1,22 @@
+--caricamento file audio
 soundTable = {
     background = audio.loadStream("sound/background.mp3"),
     jump = audio.loadSound( "sound/jump.mp3" ),
     click = audio.loadSound( "sound/click.mp3" ), --menu click
-    run = audio.loadStream("sound/ice.mp3"),
-    water = audio.loadSound("sound/splash.mp3")
+    run = audio.loadStream("sound/run.mp3"),
+    water = audio.loadSound("sound/water.mp3")
 }
+
+--inserimento libreria composer
 local composer = require "composer"
 
+-- rimuovo la bar di Android 
 display.setStatusBar(display.HiddenStatusBar)
--- Removes bottom bar on Android 
 if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
   native.setProperty( "androidSystemUiVisibility", "lowProfile" )
 else
   native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
 end
 
-
+--avvio scena menu
 composer.gotoScene( "scene.menu" )
